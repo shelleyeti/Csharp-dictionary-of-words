2ec816235779d2334182d75364fa17fe87ee77eb
Shelley Arnold <shelle.kay@gmail.com>
2019-07-09 10:03:59 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dictionary_of_words
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      /*
+    Create a dictionary with key value pairs to
+    represent words (key) and its definition (value)
+    */
+      Dictionary<string, string> wordsAndDefinitions = new Dictionary<string, string>()
+      { { "Emily", "Space Fan" }, { "Addam", "Podcast Fan" }
+      };
+
+      // Add several more words and their definitions
+      wordsAndDefinitions.Add("Awesome", "The feeling of students when they are learning C#");
+      wordsAndDefinitions.Add("Heather", "Danish Fan");
+
+      /*
+          Use square brackets to get the definition of two of the
+          words and then output them to the console
+      */
+      Console.WriteLine(wordsAndDefinitions["Heather"]);
+      Console.WriteLine(wordsAndDefinitions["Awesome"]);
+      /*
+          Below, loop over the wordsAndDefinitions dictionary to get the following output:
+              The definition of (WORD) is (DEFINITION)
+              The definition of (WORD) is (DEFINITION)
+              The definition of (WORD) is (DEFINITION)
+      */
+      foreach (KeyValuePair<string, string> word in wordsAndDefinitions)
+      {
+        Console.WriteLine($"The definition of {word.Key} is {word.Value}");
+      }
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Practice: Dictionary of Words
+
+## Instructions
+You are going to buid a C# Dictionary to represent an actual dictionary. Each KeyValuePair within the Dictionary will contain a single word as the key, and a definition as the value. Below is some starter code. You need to add a few more words and definitions to the dictionary.
+
+After you have added them, use square bracket notation to output the definition of two of the words to the console.
+
+Lastly, use the foreach loop to iterate over the KeyValuePairs and display the entire dictionary to the console.
+
+### Results will appear in the console
